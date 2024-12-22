2d8796a6b6a96a3159bf15b22cd407a4674ce07f
Hamdan Wijaya <hamdan@seclab.id>
2024-12-22 22:31:55 +0700

    build(next): add env configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_DATABASE: process.env.DB_DATABASE,
+  },
 };
 
 export default nextConfig;

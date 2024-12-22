5ab74777c27056bfcbad353ba33d87915bbc70f4
Hamdan Wijaya <hamdan@seclab.id>
2024-12-22 23:37:49 +0700

    chore: add public history type

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -42,3 +42,33 @@ export const responseTemplate = <T, E>(
     error,
   };
 };
+
+export type History = {
+  id: number;
+  px_name: string;
+  diagnosis: string;
+  q1: number;
+  q2: number;
+  q3: number;
+  q4: number;
+  q5: number;
+  q6: number;
+  q7: number;
+  q8: number;
+  q9: number;
+  q10: number;
+  q11: number;
+  q12: number;
+  q13: number;
+  q14: number;
+  q15: number;
+  q16: number;
+  q17: number;
+  q18: number;
+  q19: number;
+  q20: number;
+  q21: number;
+  q22: number;
+  q23: number;
+  q24: number;
+};
